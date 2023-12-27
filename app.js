f4f9b64119a4d2f277791cf5a1d32ea9f9771750
viveroa2291 <viveroa2291@uwec.edu>
2023-12-27 01:46:49 -0600

    Committing my first digest watch functions

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    angular.module('CounterApp', []) 
+    .controller('CounterController', CounterController);
+
+    CounterController.$inject = ['$scope'];
+    function CounterController($scope) {
+        $scope.onceCounter = 0; 
+        $scope.counter = 0;
+
+        $scope.showNumberOfWatchers = function () {
+            console.log('# of Watchers: ' + $scope.$$watchersCount);
+        };
+        $scope.countOnce = function () {
+            $scope.onceCounter = 1;
+        };   
+        $scope.upCounter = function () {
+            $scope.counter++;
+        };
+
+        $scope.$watch('onceCounter', function(newValue, oldValue ) {
+            console.log("onceCounter old value: ", oldValue);
+            console.log("onceCounter new value: ", newValue);
+        });
+        $scope.$watch('counter', function(newValue, oldValue ) {
+            console.log("old value: ", oldValue);
+            console.log("new value: ", newValue);
+        });
+    }
+})();
